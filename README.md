72aac1050d0741bd695a02ad43bce6bda02933ec
Unknown Bliss <m@michaelcullum.com>
2012-08-16 19:36:22 +0100

    Adding starts of Development Tools

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dev Tools Bundle
+
+This adds numerous tools to assist you in development.
+
+It was originally developed for use on the new phpBB Symfony Website but was then open sourced.
---
+++ phpBBDevToolsBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * @package phpBBDevToolsBundle
+ * @copyright (c) 2012 phpBB Group
+ * @license http://opensource.org/licenses/gpl-3.0.php GNU General Public License v3
+ * @author Unknown Bliss
+ * 
+ */
+
+namespace phpBB\DevToolsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class phpBBDevToolsBundle extends Bundle
+{
+}
